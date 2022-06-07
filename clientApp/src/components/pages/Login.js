7dafe50747403aa6f05cf083e13a58bf9ba83293
Archivaldi <artur.markov1860@gmail.com>
2022-06-07 16:35:01 +0300

    jwt access/refresh tokens are implemeted

--- clientApp/src/components/pages/Login.js
+++ clientApp/src/components/pages/Login.js
@@ -1,9 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 const Login = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(undefined);
+    const [user, setUser] = useState(null);
 
     const handleChange = (e) => {
         const value = e.target.value;
@@ -11,20 +14,62 @@ const Login = (props) => {
         id === 'email' ? setEmail(value) : setPassword(value);
     };
 
+    const refreshToken = async () => {
+        try {
+            if (user) {
+                const response = await axios.post("http://localhost:8080/user/refresh", { refreshToken: user.refreshToken });
+                setUser({
+                    ...user,
+                    accessToken: response.data.accessToken,
+                    refreshToken: response.data.refreshToken
+                })
+                return response.data;
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(
+        async (config) => {
+            const currentDate = new Date();
+            const decodedToken = jwt_decode(user.accessToken);
+            if (decodedToken.exp * 1000 < currentDate.getTime()) {
+                const data = await refreshToken();
+                config.headers.authorization = `Bearer ${data.accessToken}`
+            };
+            return config;
+        }, (error) => {
+            return Promise.reject(error);
+        }
+    );
+
     const submit = async () => {
         if (email && password) {
-            const response = await fetch("http://localhost:8080/user/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({email, password})
-            });
-            const responseData = await response.json();
+            const response = await axios.post("http://localhost:8080/user/login", { email, password });
+            setUser(response.data);
         } else {
-            
+            console.log("Email and Password are required to log in")
             setError("Email and Password are required to log in");
         }
+    };
+
+    const logout = async () => {
+        try {
+            if (user) {
+                await axiosJWT.post("http://localhost:8080/user/logout", { refreshToken: user.refreshToken }, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        'authorization': `Bearer ${user.accessToken}`
+                    },
+                });
+                setUser(null);
+            }
+        } catch (e) {
+            console.log(e);
+        };
     };
 
     return (
@@ -34,6 +79,13 @@ const Login = (props) => {
             <label>Password: </label>
             <input value={password} id="password" onChange={(e) => handleChange(e)} />
             <button onClick={submit}>Submit</button>
+            <button onClick={logout}>Logout</button>
+            {
+                user &&
+                <div>
+                    {JSON.stringify(user, null, 2)}
+                </div>
+            }
         </div>
     );
 };
--- server/middlewares/headers.js
+++ server/middlewares/headers.js
@@ -1,6 +1,6 @@
 const headers = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     next();
 };
 
