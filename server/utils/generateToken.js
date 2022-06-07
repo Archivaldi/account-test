85c856d4850a614fde73f0d2ea5fa852e4173625
Archivaldi <artur.markov1860@gmail.com>
2022-06-07 20:50:35 +0300

    expire 24h

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (userInfo, type) => {
     const token = type === "access"
-        ? jwt.sign({ id: userInfo.id }, process.env.JWT_SECRET, { expiresIn: "15s" })
+        ? jwt.sign({ id: userInfo.id }, process.env.JWT_SECRET, { expiresIn: "24h" })
         : jwt.sign({ id: userInfo.id }, process.env.JWT_REFRESH_SECRET);
     return token;
 };
