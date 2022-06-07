a315f7510ee55e2261749fbec34c2255033b6db8
Archivaldi <artur.markov1860@gmail.com>
2022-06-07 12:44:29 +0300

    initial commit for dev branch

--- server/server.js
+++ server/server.js
@@ -6,6 +6,4 @@ const PORT = process.env.PORT || 8080;
 
 app.use(router);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ userFakeData.js
@@ -0,0 +1,16 @@
+const users = [
+    {
+        id: '1',
+        email: "test@test.com",
+        password: 'test1234',
+        isAdmin: true
+    },
+    {
+        id: '2',
+        email: "test1@test1.com",
+        password: 'test1234',
+        isAdmin: false
+    }
+];
+
+module.exports = users;
