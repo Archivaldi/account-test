3a347ce000f23958bcd32177993e327a2544a947
Archivaldi <artur.markov1860@gmail.com>
2022-06-10 14:14:45 +0300

    config

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.use(router);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "./client/build")))
 };
-
 if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "./client/build/index.html"))
